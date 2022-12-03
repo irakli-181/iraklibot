898a7baeb3bd6cce84963a3c8a58fb4137ba055b
irakli-181 <irakli.khizanishvili18@gmail.com>
2022-12-04 01:35:35 +0400

    Update greetings.js

--- phases/greetings.js
+++ phases/greetings.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import {FuzzySet} from 'fuzzyset.js';
-=======
 import {FuzzySet} from '../fuzzyset.js';
->>>>>>> d81c6f5d880bd44c9c58dab3255702b5450ac76b
 export var list = FuzzySet([
 'გამარჯობა',
 'გამარჯობა ირაკლი',
